1b53ad2d9f91418deec0d7808c67c2255e87ea73
maribelcuales <maribelcuales@gmail.com>
2018-07-21 01:35:53 +0800

    passed heapsort

--- data_structures/ex_2/heap.py
+++ data_structures/ex_2/heap.py
@@ -1,12 +1,28 @@
+def heapify(arr, n, i):
+  largest = i  #root 
+  left = 2 * i + 1
+  right = 2 * i + 2
+
+  if left < n and arr[i] < arr[left]:
+    largest = left
+  
+  if right < n and arr[largest] < arr[right]:
+    largest = right
+
+  if largest != i:
+    arr[i], arr[largest] = arr[largest], arr[i] #swap
+
+    heapify(arr, n, largest)
+
 def heapsort(arr):
   n = len(arr)
 
   for i in range(n, -1, -1):
-    _max_child(arr, n, i)
+    heapify(arr, n, i)
 
   for i in range(n - 1, 0, -1):
-    arr[i], arr[0] = arr[0], arr[i]
-    _max_child(arr, i, 0)
+    arr[i], arr[0] = arr[0], arr[i] #swap
+    heapify(arr, i, 0)
 
 class Heap:
   def __init__(self):
